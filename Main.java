e6f09656473fddad097752f6fef7ee3d7d5c63e7
delannoykurt <delannoykurt666@gmail.com>
2022-11-24 23:03:17 +0100

    add file whith the first code of poo

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
 	public static void main(String[] args){
-		System.out.println("first poo in java.");
+		int number = 12;
+		List<Integer> number = Arrays.asList(1,2,3,4,5,6);
+		number.stream().map(v => v + 1).forEach(System.out::println);
 	}
 }
